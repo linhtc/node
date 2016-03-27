559810b00561566ab762c90c9195aba8e3d7fef6
root <root@linhtc.(none)>
2016-03-27 20:11:47 +0700

    Move db

--- callcenter/service/configs/config.php
+++ callcenter/service/configs/config.php
@@ -1,13 +1,8 @@
 <?php
-define('DB_HOST', 'bef6b79c70211c3276b05deefcaf1947b0df66f5.rackspaceclouddb.com');
-define('DB_NAME', 'gcs' );
-define('DB_USER', 'gcsuser' );
-define('DB_PASS', 'ITlt$6$4GDS' );
-
-// define('DB_HOST', '6737515899afa4db1f0bb689f16c5ea56b4185d6.rackspaceclouddb.com');
-// define('DB_NAME', 'test01' );
-// define('DB_USER', 'reader' );
-// define('DB_PASS', 'dAtaB4s3$' );
+define('DB_HOST', 'se');
+define('DB_NAME', 'se' );
+define('DB_USER', 'se' );
+define('DB_PASS', 'se' );
 
 define('DB_TYPE', 'mysql');
 define('DB_PORT', '3306');
